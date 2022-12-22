8410b735f1b4876a3d4722121ce6f4bcf3b58d3f
[AlyarQuliyev] <[quliyev20@gmail.com]>
2022-12-22 14:44:01 +0400

    cv-react-app

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Experience() {
+  return (
+    <div className="card">
+        <div className="card-header">
+          <h3>
+            <i className="fa-solid fa-briefcase"></i>Experience
+          </h3>
+        </div>
+        <div className="card-body">
+          <ul>
+            <li>
+              <span>
+                Programmer
+                <br />
+              </span>
+              <small>Company</small>
+            </li>
+            <li>
+              <span>
+                1C Programmer
+                <br />
+              </span>
+              <small>Company</small>
+              <small>Jul 2012 - Feb 2017</small>
+            </li>
+          </ul>
+        </div>
+      </div>
+  );
+}
+export default Experience;
